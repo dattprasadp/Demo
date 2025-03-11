81ae3224e28d73cb9b84cf61d076b4e3fa832f8a
Datta <dattprasad.pisal@yash.com>
2025-03-11 18:16:36 +0530

    AMER develop team commit

---
+++ src/test/java/com/yash/api/rough/primeNumber.java
@@ -0,0 +1,35 @@
+package com.yash.api.rough;
+
+import java.util.Scanner;
+
+public class primeNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input the number to check
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+
+    // Method to check if a number is prime
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
